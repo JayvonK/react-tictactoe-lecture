b538efdb471f1197d984e1b777da353192c24628
JayvonK <jaykhuth@gmail.com>
2024-03-04 10:40:06 -0800

    more code for board component

--- src/Components/Board/BoardComponent.js
+++ src/Components/Board/BoardComponent.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import SquareComponent from '../Square/SquareComponent';
 
 const BoardComponent = () => {
-  return (
-    <div>
-      
+
+    const squareComps = [];
+
+    SquareComponent.forEach((value, i) => {
+        squareComps.push(
+            <SquareComponent value={value} key={i} onClick={() => onClick(i)} />
+        )
+    })
+    return squareComps;
+};
+
+return (
+    <div className='board'>
+        <div>
+            <div>{squareComps().slice(0, 3)}</div>
+            <div>{squareComps().slice(3, 6)}</div>
+            <div>{squareComps().slice(6, 9)}</div>
+        </div>
     </div>
-  )
-}
+)
+
 
 export default BoardComponent
