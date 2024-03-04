e9664698b97ed2db9219194350e9024e9feb7b1a
JayvonK <jaykhuth@gmail.com>
2024-03-04 10:43:13 -0800

    save

--- src/Components/Board/BoardComponent.js
+++ src/Components/Board/BoardComponent.js
@@ -3,25 +3,27 @@ import SquareComponent from '../Square/SquareComponent';
 
 const BoardComponent = () => {
 
-    const squareComps = [];
+    const renderSquares = () => {
+        const squareComps = [];
 
-    SquareComponent.forEach((value, i) => {
-        squareComps.push(
-            <SquareComponent value={value} key={i} onClick={() => onClick(i)} />
-        )
-    })
-    return squareComps;
-};
+        SquareComponent.forEach((value, i) => {
+            squareComps.push(
+                <SquareComponent value={value} key={i} onClick={() => onClick(i)} />
+            )
+        })
+        return squareComps;
+    }
+
 
 return (
     <div className='board'>
-        <div>
-            <div>{squareComps().slice(0, 3)}</div>
-            <div>{squareComps().slice(3, 6)}</div>
-            <div>{squareComps().slice(6, 9)}</div>
+        <div className='boardRow'>
+            <div>{renderSquares().slice(0, 3)}</div>
+            <div>{renderSquares().slice(3, 6)}</div>
+            <div>{renderSquares().slice(6, 9)}</div>
         </div>
     </div>
 )
-
+};
 
 export default BoardComponent
