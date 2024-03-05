a12fed0576073615ff319a9427e6816eda3860f4
JayvonK <jaykhuth@gmail.com>
2024-03-05 02:34:35 -0800

    studying and formatting

--- src/Components/Board/BoardComponent.js
+++ src/Components/Board/BoardComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SquareComponent from '../Square/SquareComponent';
 
-const BoardComponent = ( { onClick, squares }) => {
+const BoardComponent = ({ onClick, squares }) => {
 
     const renderSquares = () => {
         const squareComps = [];
@@ -15,15 +15,15 @@ const BoardComponent = ( { onClick, squares }) => {
     }
 
 
-return (
-    <div className='board'>
-        <div className='boardRow'>
-            <div>{renderSquares().slice(0, 3)}</div>
-            <div>{renderSquares().slice(3, 6)}</div>
-            <div>{renderSquares().slice(6, 9)}</div>
+    return (
+        <div className='board'>
+            <div className='boardRow'>
+                <div>{renderSquares().slice(0, 3)}</div>
+                <div>{renderSquares().slice(3, 6)}</div>
+                <div>{renderSquares().slice(6, 9)}</div>
+            </div>
         </div>
-    </div>
-)
+    )
 };
 
 export default BoardComponent
--- src/Components/Square/SquareComponent.js
+++ src/Components/Square/SquareComponent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SquareComponent = ({ value, onClick}) => {
+const SquareComponent = ({ value, onClick }) => {
   return (
     <button className='square' onClick={onClick}>{value}</button>
   )
